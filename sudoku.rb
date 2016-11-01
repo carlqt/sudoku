8f9cee554146446a2e1163f7a7d4e1bc06cd11c6
Carl Tablante <carlwilliam.tablante@gmail.com>
2016-11-01 09:08:24 +0800

    added gemfile and rspec

--- sudoku.rb
+++ sudoku.rb
@@ -1,5 +1,4 @@
 require 'set'
-require 'pry'
 require_relative './sudoku/base_grid'
 require_relative './sudoku/solutions_grid'
 require_relative './sudoku/grid'
@@ -11,21 +10,23 @@ class SudokuSolver
   end
 
   def solve
-    contstraint = Sudoku::ConstraintPropagation.new(@sudoku_grid)
-    constraint.execute!
-
-    if @sudoku_grid.solved?
-      puts @sudoku_grid.solutions_grid
-    else
-      constraint_copy = constraint.dup
+    hard2  = '.................................................................................'
+    solutions_grid = @sudoku_grid.solutions_grid
+    # initialize solutions_grid
+    @sudoku_grid.parsed_grid.each do |square, value|
+      if Sudoku::Grid.digits_string.include?(value) && solutions_grid.assign_and_propagate!(square, value)
+        next
+      end
     end
+
+    solutions_grid.search(hard2)
   end
 end
 
 grid1  = '003020600900305001001806400008102900700000008006708200002609500800203009005010300'
 hard1 = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
 
-sgrid = Sudoku::Grid.new(hard1)
+sgrid = Sudoku::Grid.new(grid1)
 # s = Sudoku::ConstraintPropagation.new(sgrid)
 # s.execute!
 sol_grid = sgrid.solutions_grid
@@ -36,5 +37,5 @@ sgrid.parsed_grid.each do |square, value|
 end
 sol_grid.search(sol_grid.grid)
 sol_grid.display
-# binding.pry
+binding.pry
 'he'
