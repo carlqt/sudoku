5b55b4d358c222d0ece9df0db9fce732dcec93aa
Carl Tablante <carlwilliam.tablante@gmail.com>
2016-11-01 12:58:14 +0800

    refactor solutions spec

--- spec/solutions_grid_spec.rb
+++ spec/solutions_grid_spec.rb
@@ -2,17 +2,20 @@ require 'spec_helper'
 
 describe Sudoku::SolutionsGrid do
   let(:input) { '003020600900305001001806400008102900700000008006708200002609500800203009005010300' }
+  let(:parsed_grid) { Sudoku::Grid.new(input).parsed_grid }
+
   describe '.initialize' do
-    subject(:grid){ Sudoku::SolutionsGrid.new }
-    it 'initializes the squares (#grid) with all possible values' do
-      grid.grid.each do |_, v|
-        expect(v).to eq '123456789'
-      end
+    subject(:grid){ Sudoku::SolutionsGrid.new(parsed_grid) }
+    it 'initializes the squares (#grid) based on input. It applies constraint propagation immediately' do
+      expect(grid.grid["A3"]).to eq 3.to_s
     end
   end
 
   describe '#unsolved_squares' do
-    subject(:grid){ Sudoku::SolutionsGrid.new.unsolved_squares }
+    let(:input) {'4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'}
+    let(:parsed_grid) { Sudoku::Grid.new(input).parsed_grid }
+
+    subject(:grid){ Sudoku::SolutionsGrid.new(parsed_grid).unsolved_squares }
     it 'returns all squares\' values that are greater than 1' do
       grid.each do |arr|
         expect(arr.last.size).to be > 1
@@ -22,23 +25,19 @@ describe Sudoku::SolutionsGrid do
 
   describe '#solved?' do
     context 'when a square has 2 or more possible value' do
-      subject(:grid){ Sudoku::SolutionsGrid.new.solved? }
+      let(:input) {'4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'}
+      let(:parsed_grid) { Sudoku::Grid.new(input).parsed_grid }
+
+      subject(:grid){ Sudoku::SolutionsGrid.new(parsed_grid).solved? }
       it 'returns false' do
         expect(grid).to eq false
       end
     end
 
     context 'when all squares has only 1 possible value' do
-      let(:input) { '237168945648597321189342576726453819394281657815779234973624768561976482472835196' }
-
-      before do
-        sudoku_grid = Sudoku::Grid.new(input)
-        allow(grid).to receive(:grid) { sudoku_grid.parsed_grid }
-      end
-
-      subject(:grid){ Sudoku::SolutionsGrid.new(input) }
+      subject(:grid){ Sudoku::SolutionsGrid.new(parsed_grid).solved? }
       it 'returns true' do
-        expect(grid.solved?(grid.grid)).to eq true
+        expect(grid).to eq true
       end
     end
   end
@@ -78,7 +77,7 @@ describe Sudoku::SolutionsGrid do
 
     it 'trys every possible values of the square until it reaches a solved state' do
       solutions_grid.search(solutions_grid.grid)
-  
+
       expect(solutions_grid.solved?).to eq true
     end
   end
